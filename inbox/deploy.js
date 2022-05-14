e5581087f5eb9227d5cbb0cd4e1d5cf2ba19394a
Narek Hakobyan <nhakobyan@vmware.com>
2022-05-14 21:55:22 +0400

    Smart contract
    Smart contract with send message functionality and simple tests.
    Compiler version 0.4.17

---
+++ inbox/deploy.js
@@ -0,0 +1,31 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile');
+
+const provider = new HDWalletProvider(
+    /* Put here your wallet mnemonic */
+    'YOUR_MNEMONIC',
+    /* Put here your project endpoint link */
+    /* 
+      To get that link you should go to infura.io. Sign in and create new project.
+      Change endpoints to Rinkeby and copy https link 
+    */
+    'YOUR_INFURA_URL'
+    );
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({
+        data: bytecode,
+        arguments: ["Hi there!"],
+      })
+      .send({ from: accounts[0], gas: "1000000" });
+      console.log('Contract deployed at address: ',inbox.options.address);
+      provider.engine.stop();
+};
+
+deploy();
