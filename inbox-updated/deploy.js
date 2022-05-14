6feab18730b68c64ba537381edda9f19ae28a9aa
Narek Hakobyan <nhakobyan@vmware.com>
2022-05-14 22:51:32 +0400

    Add version for solidity compiler 0.8.9

---
+++ inbox-updated/deploy.js
@@ -0,0 +1,32 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+ 
+const { abi, evm } = require('./compile');
+ 
+provider = new HDWalletProvider(
+  /* Put here your wallet mnemonic */
+  'YOUR_MNEMONIC',
+  /* Put here your project endpoint link */
+  /* 
+      To get that link you should go to infura.io. Sign in and create new project.
+      Change endpoints to Rinkeby and copy https link 
+  */
+  'YOUR_INFURA_URL'
+);
+ 
+const web3 = new Web3(provider);
+ 
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+ 
+  console.log('Attempting to deploy from account', accounts[0]);
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+ 
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+ 
+deploy();
